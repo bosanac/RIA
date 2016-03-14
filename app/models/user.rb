3c6d4a90b73a6b8769c6bb7ffb41c4219a9b3052
bosanac <turcinovic@gmail.com>
2016-03-14 02:54:13 -0700

    Dodani u modelu User atributi za password_secure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ActiveRecord::Base
   before_save {self.email = email.downcase}
+  
   add_index :users, :email, unique: true
 
-  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
+  has_secure_password
+  
+  validates :password, presence: true, length: { minimum: 6 }
+    
   
   def change
       create_table :users do |t|
