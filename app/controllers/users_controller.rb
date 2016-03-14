c5bc19b65b64d7c49b201f6f2ec7872b9d3622be
bosanac <turcinovic@gmail.com>
2016-03-14 05:21:01 -0700

    Zavrsen singup korisnika

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,24 @@ class UsersController < ApplicationController
   end
   
   def create
+    @user = User.new(user_params)
     
+    if @user.save
+    # Uspjesno popunjen i sacuvan korisnik
+      flash[:success] = "Dobro dosli, uspjesno ste kreirali korisnicki nalog!"
+      redirect_to @user
+    else
+    # Nije uspjesno sacuvan korisnik
+    flash[:success]
+    render 'new'
+    end
   end
+  
+  
+  private
+
+  def user_params 
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
   
 end
