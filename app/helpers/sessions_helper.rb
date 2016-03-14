78f3fce937a2b8e3dceacd259d4584c844582ca7
bosanac <turcinovic@gmail.com>
2016-03-14 07:08:06 -0700

    dodani kontroleri i helper za login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+  
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+    def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
 end
