68ea5c2bd09d65603b6070f8572eb9ff4cb7f2b0
bosanac <turcinovic@gmail.com>
2016-03-14 00:42:47 -0700

    Prepravljeni nazivi i tekstovi linkova i natpisa - Home stranice

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
 =begin
   root 'stranice#home'
   get  'stranice/help'
@@ -8,6 +10,8 @@ Rails.application.routes.draw do
 =end
 
 Rails.application.routes.draw do
+  get 'users/new'
+
   root             'stranice#home'
   get 'help'    => 'stranice#help'
   get 'about'   => 'stranice#about'
